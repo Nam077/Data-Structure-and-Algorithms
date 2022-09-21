aef71fd7af0b2a4b7f3c3a5cba06fd27277d98c7
Nguyễn Văn Nam <87227884+Nam077@users.noreply.github.com>
2022-09-21 12:28:13 +0700

    string length

---
+++ string_length.py
@@ -0,0 +1,8 @@
+def string_length(string):
+    return len(string)
+
+
+if __name__ == '__main__':
+    print("Enter string: ")
+    string_input = input()
+    print("Length =", string_length(string_input))
