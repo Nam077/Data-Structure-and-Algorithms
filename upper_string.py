f4640f689b1ffed44004257157ce70cb721e00fe
Nguyễn Văn Nam <87227884+Nam077@users.noreply.github.com>
2022-09-21 13:11:49 +0700

    upper_string

---
+++ upper_string.py
@@ -0,0 +1,8 @@
+def upper_string(string):
+    return string.upper()
+
+
+if __name__ == '__main__':
+    print("Enter string: ")
+    string_input = input()
+    print("Upper string =", upper_string(string_input))
